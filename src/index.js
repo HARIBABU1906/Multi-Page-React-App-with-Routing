aceda88ac4cba1642656b922f21704196151ac02
HARIBABU.D <148314058+HARIBABU1906@users.noreply.github.com>
2025-12-28 20:11:39 +0530

    Replace HashRouter with Routes and Route components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
+import { Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/contact" element={<Contact />} />
+</Routes>
